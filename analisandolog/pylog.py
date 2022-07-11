8a9c220734db5c9c2efb96a2bf2aa67f9baf331a
Julio <gaspardi.dev@gmail.com>
2022-07-10 23:20:01 -0300

    Requisicoes por Dia X

--- analisandolog/pylog.py
+++ analisandolog/pylog.py
@@ -1,20 +1,22 @@
+from pickletools import read_int4
 import sys
 
-log = open('access.log','r')
+from requests import request
+
 
 ## metodo que tira caracteres especiais
 def takeOff(i):
-    specialChars = ['[',']','(',')']
+    specialChars = ['[',']','(',')',' ']
     for specialChar in specialChars:
        i = i.replace(specialChar,'')
     return i 
-
+## Metodo que mostra quantas requisiçoes houveram por IP
 def ipCountRequest(log):
     lines = log.readlines()
     ipsSource = []
     ipsFoundNoRepeat = []
     for line in lines:
-        ##quebrando a linha e jogando-a para uma lista 
+        ##quebrando a linha e dividindo o conteúdo entre espaços 
         lineColumn = line.split(' ')
 
         ##Pegando o iP de cada requisição e jogando o mesmo para uma lista X para controle.
@@ -27,8 +29,28 @@ def ipCountRequest(log):
             
     ##Consultando quantas vezes cada ip apareceu. Olha cada ip da lista Y (onde estão armezanados os IPs, sem repetir)
     ##e verificando quantas vezes o mesmo apareceu na lista X(Que salva o IP baseado nas requisições mesmo repetindo ou não)
+    print('--------TOTAL REQUESTS--------')
+    ipsFoundNoRepeat.sort()
     for ip in range(0,len(ipsFoundNoRepeat)):
-        print(ipsFoundNoRepeat[ip] + ' - requests = ' + str(ipsSource.count(ipsFoundNoRepeat[ip])))
+        print(ipsFoundNoRepeat[ip] + ' = ' + str(ipsSource.count(ipsFoundNoRepeat[ip])))
+## Metodo que mostra requisiçoes feita que ocorreram em uma data
+def requestPerDay(log):
+    lines = log.readlines()
+    data = '11/Feb/2015'
+    for line in lines:
+        line = line.split(' ')
+        # print(line)
+        # print(line[3])
+        
+        datalog = line[3][1:12]
+        if data == datalog:
+            print(line,end=' ')
+            print()
+    
 
-ipCountRequest(log)
+log = open('access.log','r')
 
+#ipCountRequest(log)
+requestPerDay(log)
+
+log.close()
